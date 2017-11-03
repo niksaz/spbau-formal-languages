12448a0922bfa1b2d081cc30faa566f5bfbcecb8
niksaz <nikitasazanovich@gmail.com>
2017-11-03 15:06:04 +0300

    OutputStream is an argument now.

--- src/main/java/ru/spbau/sazanovich/nikita/proof/Main.java
+++ src/main/java/ru/spbau/sazanovich/nikita/proof/Main.java
@@ -7,23 +7,36 @@ import ru.spbau.sazanovich.nikita.proof.grammar.ContextFreeGrammar;
 import ru.spbau.sazanovich.nikita.proof.grammar.ContextFreeGrammarParser;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
+    if (args.length != 3) {
+      System.out.println(
+          "Incorrect number of arguments.There should 3 arguments: " +
+          "automatonPath grammarPath outputPath");
+      return;
+    }
+    String automatonPath = args[0];
+    String grammarPath = args[1];
+    String outputPath = args[2];
     try {
       MutableGraph automaton =
-          Parser.read(new FileInputStream("src/test/resources/automaton.dot"));
+          Parser.read(new FileInputStream(automatonPath));
       ContextFreeGrammar grammar =
-          ContextFreeGrammarParser.read(new FileInputStream("src/test/resources/grammar.txt"));
+          ContextFreeGrammarParser.read(new FileInputStream(grammarPath));
       ContextFreeGrammar normalizedGrammar = grammar.toChomskyNormalForm();
 
       IntersectionGrammarBuilder intersectionBuilder =
           new IntersectionGrammarBuilder(normalizedGrammar, automaton);
       ContextFreeGrammar intersectionGrammar = intersectionBuilder.build();
-      intersectionGrammar.printTo(System.out);
+      intersectionGrammar.printTo(new PrintStream(new FileOutputStream(outputPath)));
     } catch (IllegalArgumentException e) {
       System.out.println("Incorrect input: " + e.getMessage());
+    } catch (IOException e) {
+      System.out.println("IO error occurred: " + e.getMessage());
     }
   }
 }
